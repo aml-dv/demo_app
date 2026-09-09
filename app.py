ddc70ac65c69e44d7ba7044938f2ffe8d6abee3f
Amal Dev <amaldev729@gmail.com>
2026-09-09 14:19:00 +0530

    connection established

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
-
+import psycopg2
+DBURL="postgresql://demo_db_06rw_user:icEAsFoU1KaAbYCOy9lFJCE1zd7rt0gc@dpg-daghj1942hec73ccp3hg-a/demo_db_06rw"
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    conn = psycopg2.connect(DBURL)
+    # Create cursor
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM students;")
+    rows = cursor.fetchone()
+    cursor.close()
+    conn.close()
+
+
+    return render_template("index.html",data=rows)
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
